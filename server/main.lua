a68acdddb8f3a8c4b57d90bcfe04ff2db8c4c9cc
Abel Gaming <lcpdpolice4939@gmail.com>
2023-03-22 01:06:05 -0400

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,18 +1,3 @@
 ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-RegisterServerEvent("esx_BodyLoot:TakeItem")
-AddEventHandler('esx_BodyLoot:TakeItem', function()
-	local xPlayer = ESX.GetPlayerFromId(source)
-	local randomNumber = math.random(1, #Config.LootItems)
-	local randomItem = Config.LootItems[randomNumber]
-
-	if xPlayer.canCarryItem(randomItem, 1) then
-		xPlayer.addInventoryItem(randomItem, 1)
-		local itemLabel = ESX.GetItemLabel(randomItem)
-		xPlayer.showNotification('You found 1 ' .. itemLabel)
-	else
-		xPlayer.showNotification('[ERROR] You do not have space for this item!')
-	end
-end)
